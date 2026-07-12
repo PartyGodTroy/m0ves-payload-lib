567a1717b066c068fce842a58b0e11a8dcd953a4
PartyGodTroy <kidveno@live.com>
2026-07-12 06:22:36 -0400

    small upgrade

--- dist/plugins/common/fields/PageFields.d.ts
+++ dist/plugins/common/fields/PageFields.d.ts
@@ -1,7 +1,6 @@
 import type { Field } from 'payload';
-export declare const getCommonPageFields: ({ include_seo, include_import_export, include_nested_doc, include_search, include_redirects, }: {
+export declare const getCommonPageFields: ({ include_seo, include_nested_doc, include_search, include_redirects, }: {
     include_seo?: boolean | undefined;
-    include_import_export?: boolean | undefined;
     include_nested_doc?: boolean | undefined;
     include_search?: boolean | undefined;
     include_redirects?: boolean | undefined;
